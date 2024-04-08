2c7592b9e788ef2508d401d3f23f964219842e62
Lluc Llull <lluc@clicktotravel.es>
2024-04-08 16:03:14 +0200

    changes section 18

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,9 +1,65 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormArray, FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   templateUrl: './switches-page.component.html',
   styles: ``
 })
-export class SwitchesPageComponent {
+export class SwitchesPageComponent implements OnInit{
+  public myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [true, Validators.required],
+    termsAndConditions: [false, Validators.requiredTrue],
+  });
+
+  public person = {
+    gender: 'F',
+    wantNotifications: false,
+  }
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit():void {
+    this.myForm.reset(this.person);
+  }
+
+  isValidField(field: string):boolean|null {
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched;
+  }
+
+  getFieldError(field: string):string|null {
+    if(!this.myForm.controls[field]) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)){
+      switch (key){
+        case 'required':
+          return 'Este campo es requerido';
+        case 'minlength':
+          return `Minimo ${errors['minlength'].requiredLength} caracters`;
+        case 'min':
+          return `El valor debe ser  ${errors['min'].min} o mayor`;
+        case 'requiredtrue':
+          return 'Debe de aceptar las condiciones de uso';
+      }
+    }
+
+    return null;
+  }
+
+  //ngSubmit
+  onSave() {
+    if(this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    const {termsAndConditions, ...newPerson} = this.myForm.value;
+
+    this.person = newPerson;
+    console.log(this.myForm.value);
+  }
+
 
 }
---
+++ src/app/shared/validators/email-validator.service.ts
@@ -0,0 +1,39 @@
+import {Injectable} from '@angular/core';
+import {AbstractControl, AsyncValidator, ValidationErrors} from "@angular/forms";
+import {delay, Observable, of} from "rxjs";
+import {verifySupportedTypeScriptVersion} from "@angular/compiler-cli/src/typescript_support";
+
+@Injectable({providedIn: 'root'})
+export class EmailValidator implements AsyncValidator{
+
+  validate(control: AbstractControl): Observable<ValidationErrors | null> {
+
+      const email = control.value;
+      const httpCallObservable = new Observable<ValidationErrors|null>((subscriber) => {
+        console.log({email});
+
+        if(email === 'lluc@gmail.com'){
+          subscriber.next({emailTaken: true});
+          subscriber.complete();
+        }
+
+        subscriber.next(null);
+        subscriber.complete();
+      });
+
+      return httpCallObservable;
+  }
+
+  // validate(control: AbstractControl): Observable<ValidationErrors | null> {
+  //
+  //   const email = control.value;
+  //   console.log({email});
+  //
+  //   return of({
+  //     emailTaken: true
+  //   }).pipe(
+  //     delay(2000)
+  //   );
+  // }
+
+}
